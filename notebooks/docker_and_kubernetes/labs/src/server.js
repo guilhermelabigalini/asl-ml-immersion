14c57704a55b416d6db014e5d3d01ae63c42af79
guilhermelabigalini <guilherme.labigalini@gmail.com>
2022-02-08 16:12:27 +0000

    backup

--- notebooks/docker_and_kubernetes/labs/src/server.js
+++ notebooks/docker_and_kubernetes/labs/src/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World!\n");
+  response.end("Hello World -v2 !" + (new Date()) + "\n");
 }
 var www = http.createServer(handleRequest);
 www.listen(8000);
